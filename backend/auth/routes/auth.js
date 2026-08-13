e96e3cd4b33a3d37563d918734ebc7682c0f4207
Anuraju669 <anurajanuraj575@gmail.com>
2026-08-13 15:54:40 +0530

    Add auth system

---
+++ backend/auth/routes/auth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+    register,
+    login
+} = require("../controllers/authController");
+
+const {
+    protect,
+    adminOnly
+} = require("../middleware/authMiddleware");
+
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+
+router.get("/profile", protect, (req, res) => {
+    res.json({
+        message: "You can access this protected route",
+        user: req.user
+    });
+});
+
+
+router.get("/admin", protect, adminOnly, (req, res) => {
+    res.json({
+        message: "Welcome Admin"
+    });
+});
+
+
+module.exports = router;
